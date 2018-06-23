3b824276d0ef36695af6dfa5ac41fc3ad35d6f0b
Ramzan Ali <purvam98@gmail.com>
2018-06-23 13:51:04 -0400

    pushing all files

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import {
+  BrowserRouter as Router,Route,Switch} from "react-router-dom";
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+      
+      <Switch>
+      <Route path="/" exact component={Books} />
+      <Route path="/books" exact component={Books} />
+      <Route path="/books/:id" exact component={Detail} />
+      <Route component={NoMatch} />
+    </Switch>
+    </div>
+
+  </Router>
+);
+
+
+export default App;
